cf2842f57d02ffbacb74aa3c32857bc26c4a5f45
nr <cocoapebles@gmail.com>
2013-11-17 19:01:12 +0900

    InsertSql

---
+++ InsertSql.php
@@ -0,0 +1,35 @@
+<?php
+class InsertSql {
+	protected $into;
+	protected $values;
+	protected $params = array();
+
+	public function into($into) {
+		$this->into = $into;
+	}
+
+	public function values($values) {
+		$this->values = $values;
+	}
+
+	public function toString() {
+		$fields = '';
+		$prepare = '';
+		foreach ($this->values as $key => $value) {
+			if (count($this->params) > 0) {
+				$fields.= ',';
+				$prepare.= ',';
+			}
+			$fields.= $key;
+			$prepareKey = ':'.$key;
+			$prepare.= $prepareKey;
+			$this->params[$prepareKey] = $value;
+		}
+		$sql = 'INSERT INTO '.$this->into.'('.$fields.') VALUES('.$prepare.')';
+		return $sql;
+	}
+
+	public function getParams() {
+		return $this->params;
+	}
+}
